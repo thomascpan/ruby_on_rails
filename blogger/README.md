be0e940eddc7e4e7cbc1bddad0a7160ae5eedc6c
Thomas C. Pan <thomascpan@gmail.com>
2015-10-06 20:49:12 -0700

    initial commit

---
+++ blogger/README.md
@@ -0,0 +1,12 @@
+Project: Getting Your Feet Wet With Rails
+
+Link: http://www.theodinproject.com/ruby-on-rails/getting-your-feet-wet
+
+Description: A simple Blogging site. 
+
+Author(s): Thomas Pan
+
+Instructions: Run site with `$rails s`. 
+
+![Blogger Home Page](/public/assets/images/screenshot.png "Blogger Home Page")
+
---
+++ blogger/app/controllers/tags_controller.rb
@@ -0,0 +1,17 @@
+class TagsController < ApplicationController
+	before_filter :require_login, only: [:destroy]
+	def show
+		@tag = Tag.find(params[:id])
+	end
+
+	def index
+		@tags = Tag.all
+	end
+
+	def destroy
+		@tag = Tag.find(params[:id])
+		@tag.destroy
+		flash.notice = "Tag '#{@tag.name}' Deleted!"
+		redirect_to tags_path
+	end
+end
